a20a6ef54bd22635947f8167de08ede249898124
Roque Giollo <giolloroque@gmail.com>
2022-04-11 17:34:54 -0300

    agregado el mock-tas

---
+++ src/app/Task.ts
@@ -0,0 +1,6 @@
+export interface Task{    //esto es la interface para el mock-task
+    id?:number;
+    text:string;
+    day:string; 
+    reminder: boolean;
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from './Task'
+
+export const TASKS:Task[] = [
+    {
+        id:1,
+        text:"Terminar el primer modulo de angular",
+        day:"agosto 5 a las 12:00",
+        reminder: true 
+    },
+    {
+        id:2,
+        text:"Hacer las compras para la cena",
+        day:"agosto 5 a las 17:00",
+        reminder: true 
+    },
+    {
+        id:3,
+        text:"Investigar sobre Boostrap",
+        day:"agosto 5 a las 19:00",
+        reminder: false
+    },
+    {
+        id:4,
+        text:"leer mi libro favorito",
+        day:"agosto 5 a las 21:00",
+        reminder: false 
+    }
+]
