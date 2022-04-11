57e734b1520fa90e1aece581a0bdf749fd3896d2
Roque Giollo <giolloroque@gmail.com>
2022-04-11 18:14:14 -0300

    add conponent: task,task-item// min 39:28

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './conponents/header/header.component';
 import { ButtonComponent } from './conponents/button/button.component';
+import { TasksComponent } from './conponents/tasks/tasks.component';
+import { TaskItemComponent } from './conponents/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
