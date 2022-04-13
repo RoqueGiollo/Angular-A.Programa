740e4cf51ac4fa8808c1c3c57d56a2b4640dfb2a
Roque Giollo <giolloroque@gmail.com>
2022-04-13 19:00:04 -0300

    remplazamos el mock por un jason con la informacion y obtenemos informacion dinamica-1:10:00

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHandler} from '@angular/common/http'; //nos permite hacer llamadas get-post 
 import {Observable, observable, of} from 'rxjs';
 import {TASKS} from '../mock-tasks';
 import { Task } from 'src/app/Task';
@@ -7,10 +8,14 @@ import { Task } from 'src/app/Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks "
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+
+    return this.http.get<Task[]>(this.apiUrl)  // el get nos devuelve la lista de tareas, a travez de apiUrl.
   }
 }
